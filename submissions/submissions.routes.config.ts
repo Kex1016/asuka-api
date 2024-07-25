94e0d055af3a88fb5bfc1b1f4b4efebcad7fdca3
cakes <cakes@haiiro.moe>
2024-07-25 22:25:11 +0200

    feat: add placeholder routes

--- submissions/submissions.routes.config.ts
+++ submissions/submissions.routes.config.ts
@@ -7,6 +7,31 @@ export class SubmissionsRoutesConfig extends CommonRoutesConfig {
   }
 
   configureRoutes(): express.Application {
-    return this.app; // TODO: this is a placeholder
+    this.app.route(`/submissions`)
+      .get((req: express.Request, res: express.Response) => {
+        res.status(200).send(`List of submissions`)
+      })
+      .post((req: express.Request, res: express.Response) => {
+        res.status(200).send(`Post to submissions`)
+      })
+
+    this.app.route(`/submissions/:submissionId`)
+      .all((req: express.Request, res: express.Response, next: express.NextFunction) => {
+        next()
+      })
+      .get((req: express.Request, res: express.Response) => {
+        res.status(200).send(`GET requested for id ${req.params.submissionId}`)
+      })
+      .put((req: express.Request, res: express.Response) => {
+        res.status(200).send(`PUT requested for id ${req.params.submissionId}`)
+      })
+      .patch((req: express.Request, res: express.Response) => {
+        res.status(200).send(`PATCH requested for id ${req.params.submissionId}`)
+      })
+      .delete((req: express.Request, res: express.Response) => {
+        res.status(200).send(`DELETE requested for id ${req.params.submissionId}`)
+      })
+
+    return this.app
   }
 }

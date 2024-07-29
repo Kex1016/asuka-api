246842f7aa4068cab1f80fa42325c147687e0f9c
cakes <cakes@haiiro.moe>
2024-07-29 18:56:54 +0200

    refactor: implement the controller and middleware into the routes

--- submissions/submissions.routes.config.ts
+++ submissions/submissions.routes.config.ts
@@ -1,37 +1,32 @@
 import express from 'express'
-import { CommonRoutesConfig } from '../common/common.routes.config.ts'
+import {CommonRoutesConfig} from '../common/common.routes.config.ts'
+import SubmissionsController from "./controllers/submissions.controller.ts";
+import SubmissionsMiddleware from "./middleware/submissions.middleware.ts";
 
 export class SubmissionsRoutesConfig extends CommonRoutesConfig {
-  constructor(app: express.Application) {
-    super(app, 'SubmissionsRoutes')
-  }
+    constructor(app: express.Application) {
+        super(app, 'SubmissionsRoutes')
+    }
 
-  configureRoutes(): express.Application {
-    this.app.route(`/submissions`)
-      .get((req: express.Request, res: express.Response) => {
-        res.status(200).send(`List of submissions`)
-      })
-      .post((req: express.Request, res: express.Response) => {
-        res.status(200).send(`Post to submissions`)
-      })
+    configureRoutes(): express.Application {
+        this.app.route(`/submissions`)
+            .get(SubmissionsController.listSubmissions)
+            .post(
+                SubmissionsMiddleware.validateRequiredSubmissionBodyFields,
+                SubmissionsMiddleware.validateMoreThanTwoSubmissionsPerUser,
+                SubmissionsController.createSubmission
+            )
 
-    this.app.route(`/submissions/:submissionId`)
-      .all((req: express.Request, res: express.Response, next: express.NextFunction) => {
-        next()
-      })
-      .get((req: express.Request, res: express.Response) => {
-        res.status(200).send(`GET requested for id ${req.params.submissionId}`)
-      })
-      .put((req: express.Request, res: express.Response) => {
-        res.status(200).send(`PUT requested for id ${req.params.submissionId}`)
-      })
-      .patch((req: express.Request, res: express.Response) => {
-        res.status(200).send(`PATCH requested for id ${req.params.submissionId}`)
-      })
-      .delete((req: express.Request, res: express.Response) => {
-        res.status(200).send(`DELETE requested for id ${req.params.submissionId}`)
-      })
+        this.app.route(`/submissions/:submissionId`)
+            .all(SubmissionsMiddleware.validateSubmissionExists)
+            .get(SubmissionsController.getSubmissionById)
+            .put(
+                SubmissionsMiddleware.validateRequiredSubmissionBodyFields,
+                SubmissionsController.put
+            )
+            .patch(SubmissionsController.patch)
+            .delete(SubmissionsController.removeSubmission)
 
-    return this.app
-  }
+        return this.app
+    }
 }

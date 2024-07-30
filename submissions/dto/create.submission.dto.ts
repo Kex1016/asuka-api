40438d6cd7a8613173dbfde79d38d4bfa6f65566
cakes <cakes@haiiro.moe>
2024-07-30 12:31:32 +0200

    refactor: prepare create submission dto for mongoose

--- submissions/dto/create.submission.dto.ts
+++ submissions/dto/create.submission.dto.ts
@@ -1,10 +1,9 @@
 export interface CreateSubmissionDto {
-  id: string;
   name: string;
   image: string;
   suggestedBy: string;
-  messageId?: string;
-  accepted?: boolean;
-  used?: boolean;
-  prev: boolean;
+  messageId?: string;  // Voting message ID
+  accepted?: boolean;  // Whether the submission was accepted
+  used?: boolean;      // Whether the submission was used
+  won: boolean;        // Whether the submission won the voting that week (default false)
 }

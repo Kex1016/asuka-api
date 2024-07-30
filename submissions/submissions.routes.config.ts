37b108acd6c2ffc8fa01e2d44914ecfd87dd101f
cakes <cakes@haiiro.moe>
2024-07-30 19:13:15 +0200

    refactor: implement the validation middleware

--- submissions/submissions.routes.config.ts
+++ submissions/submissions.routes.config.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import {CommonRoutesConfig} from '../common/common.routes.config.ts'
 import SubmissionsController from "./controllers/submissions.controller.ts";
 import SubmissionsMiddleware from "./middleware/submissions.middleware.ts";
+import {body} from "express-validator";
+import BodyValidationMiddleware from "../common/middleware/body.validation.middleware.ts";
 
 export class SubmissionsRoutesConfig extends CommonRoutesConfig {
     constructor(app: express.Application) {
@@ -12,7 +14,13 @@ export class SubmissionsRoutesConfig extends CommonRoutesConfig {
         this.app.route(`/submissions`)
             .get(SubmissionsController.listSubmissions)
             .post(
-                SubmissionsMiddleware.validateRequiredSubmissionBodyFields,
+                body('name').isString(),
+                body('image').isString(),
+                body('suggestedBy').isString().isLength({
+                    min: 18,
+                    max: 18
+                }).withMessage('Must be a valid Discord user ID'),
+                BodyValidationMiddleware.verifyBodyFieldsErrors,
                 SubmissionsMiddleware.validateMoreThanTwoSubmissionsPerUser,
                 SubmissionsController.createSubmission
             )
@@ -21,10 +29,25 @@ export class SubmissionsRoutesConfig extends CommonRoutesConfig {
             .all(SubmissionsMiddleware.validateSubmissionExists)
             .get(SubmissionsController.getSubmissionById)
             .put(
-                SubmissionsMiddleware.validateRequiredSubmissionBodyFields,
+                body('name').isString(),
+                body('image').isString(),
+                body('suggestedBy').isString().isLength({
+                    min: 18,
+                    max: 18
+                }).withMessage('Must be a valid Discord user ID'),
+                BodyValidationMiddleware.verifyBodyFieldsErrors,
                 SubmissionsController.put
             )
-            .patch(SubmissionsController.patch)
+            .patch(
+                body('name').isString().optional(),
+                body('image').isString().optional(),
+                body('suggestedBy').isString().isLength({
+                    min: 18,
+                    max: 18
+                }).withMessage('Must be a valid Discord user ID').optional(),
+                BodyValidationMiddleware.verifyBodyFieldsErrors,
+                SubmissionsController.patch
+            )
             .delete(SubmissionsController.removeSubmission)
 
         return this.app

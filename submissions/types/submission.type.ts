78737c2792539e2c8d7a22b040cdfc467f7fbc5a
cakes <cakes@haiiro.moe>
2024-07-30 12:51:28 +0200

    feat: add typing for submission

---
+++ submissions/types/submission.type.ts
@@ -0,0 +1,10 @@
+export type SubmissionType = {
+    _id: string
+    name: string
+    image: string
+    suggestedBy: string
+    messageId: string
+    accepted: boolean
+    used: boolean
+    won: boolean
+}

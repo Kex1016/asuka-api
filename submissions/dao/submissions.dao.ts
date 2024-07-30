941aac45ecb711379473aa41b71389419a182f95
cakes <cakes@haiiro.moe>
2024-07-30 12:32:05 +0200

    feat: hook up mongoose to the submissions DAO

--- submissions/dao/submissions.dao.ts
+++ submissions/dao/submissions.dao.ts
@@ -1,69 +1,74 @@
-import { type CreateSubmissionDto } from '../dto/create.submission.dto.ts'
-import { type PatchSubmissionDto } from '../dto/patch.submission.dto.ts'
-import { type PutSubmissionDto } from '../dto/put.submission.dto.ts'
+import mongooseService from "../../common/services/mongoose.service.ts";
 
-import { nanoid } from 'nanoid'
+import {type CreateSubmissionDto} from '../dto/create.submission.dto.ts'
+import {type PatchSubmissionDto} from '../dto/patch.submission.dto.ts'
+import {type PutSubmissionDto} from '../dto/put.submission.dto.ts'
+
+import {nanoid} from 'nanoid'
 import debug from 'debug'
 
 const log: debug.IDebugger = debug('app:submissions-dao')
 
 class SubmissionsDao {
-  submissions: Array<CreateSubmissionDto> = []
+    Schema = mongooseService.getMongoose().Schema;
+    submissionSchema = new this.Schema({
+        _id: String,
+        name: String,
+        image: String,
+        suggestedBy: String,
+        messageId: String,
+        accepted: Boolean,
+        used: Boolean,
+        won: Boolean,
+    }, {id: false});
 
-  constructor() {
-    log('Created new instance of SubmissionsDao')
-  }
+    Submission = mongooseService.getMongoose().model('Submissions', this.submissionSchema);
 
-  async addSubmission(submission: CreateSubmissionDto) {
-    submission.id = nanoid() // TODO: Change the DB to have string IDs
-    this.submissions.push(submission)
-    return submission.id
-  }
+    submissions: Array<CreateSubmissionDto> = []
 
-  async getSubmissions() {
-    return this.submissions
-  }
-
-  async getSubmissionById(submissionId: string) {
-    return this.submissions.find((submission: { id: string }) => submission.id === submissionId)
-  }
-
-  async putSubmissionById(submissionId: string, submission: PutSubmissionDto) {
-    const objIndex = this.submissions.findIndex((obj: { id: string }) => obj.id === submissionId)
-    this.submissions.splice(objIndex, 1, submission)
-    return `${submission.id} updated via PUT`
-  }
-
-  async patchSubmissionById(submissionId: string, submission: PatchSubmissionDto) {
-    const objIndex = this.submissions.findIndex((obj: { id: string }) => obj.id === submissionId)
-    let currentSubmission = this.submissions[objIndex]
-    const allowedPatchFields = [
-      'name',
-      'image',
-      'messageId',
-      'accepted',
-      'used',
-      'prev'
-    ]
-    for (let field of allowedPatchFields) {
-      if (field in submission) {
-        // @ts-ignore
-        currentSubmission[field] = submission[field]
-      }
+    constructor() {
+        log('Created new instance of SubmissionsDao')
     }
-    this.submissions.splice(objIndex, 1, currentSubmission)
-    return `${submission.id} patched`
-  }
 
-  async removeSubmissionById(submissionId: string) {
-    const objIndex = this.submissions.findIndex((obj: { id: string }) => obj.id === submissionId)
-    this.submissions.splice(objIndex, 1)
-    return `${submissionId} removed`
-  }
+    async addSubmission(submissionFields: CreateSubmissionDto) {
+        const submissionId = nanoid()
+        const submission = new this.Submission({
+            _id: submissionId,
+            ...submissionFields
+        });
 
-  async getSubmissionsByUserId(suggestedBy: string) {
-    return this.submissions.filter((submission: { suggestedBy: string }) => submission.suggestedBy === suggestedBy)
-  }
+        // TODO: Add downloading image to local storage
+
+        await submission.save()
+        return submissionId
+    }
+
+    async getSubmissions(limit = 25, page = 0) {
+        return this.Submission.find()
+            .limit(limit)
+            .skip(limit * page)
+            .exec()
+    }
+
+    async getSubmissionById(submissionId: string) {
+        return this.Submission.findById(submissionId).exec()
+    }
+
+    async getSubmissionsByUserId(suggestedBy: string) {
+        return this.Submission.find({suggestedBy: suggestedBy}).exec()
+    }
+
+    async updateSubmissionById(submissionId: string, submissionFields: PatchSubmissionDto | PutSubmissionDto) {
+        return await this.Submission.findOneAndUpdate(
+            {_id: submissionId},
+            {$set: submissionFields},
+            {new: true}
+        ).exec()
+    }
+
+    async removeSubmissionById(submissionId: string) {
+        return this.Submission.deleteOne({_id: submissionId}).exec()
+    }
 }
 
 export default new SubmissionsDao()

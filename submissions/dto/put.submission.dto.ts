cb3aa1af7e73f44d6d59e7bc77213af8cc8eeaea
cakes <cakes@haiiro.moe>
2024-07-30 12:31:41 +0200

    refactor: prepare put submission dto for mongoose

--- submissions/dto/put.submission.dto.ts
+++ submissions/dto/put.submission.dto.ts
@@ -1,10 +1,9 @@
 export interface PutSubmissionDto {
-  id: string;
   name: string;
   image: string;
   suggestedBy: string;
   messageId: string;
   accepted: boolean;
   used: boolean;
-  prev: boolean;
+  won: boolean;
 }
